9a4ec287fe90b79dbab5f1d92209b04bb9a6d21c
Paul <poliverjr@hotmail.com>
2016-01-30 20:16:42 -0500

    final(probably)

--- app/src/main/java/com/jr/poliv/temperatureconverter/Celsius_to_Fahrenheit.java
+++ app/src/main/java/com/jr/poliv/temperatureconverter/Celsius_to_Fahrenheit.java
@@ -1,11 +1,16 @@
 package com.jr.poliv.temperatureconverter;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Celsius_to_Fahrenheit extends AppCompatActivity {
 
@@ -16,14 +21,68 @@ public class Celsius_to_Fahrenheit extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                inputManager.hideSoftInputFromWindow((null == getCurrentFocus()) ? null : getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
+                display(C_to_F(acceptvar()), 'F');
             }
         });
+
+
+
+    }
+    double F_to_C(double var)
+    {
+        double result;
+        result = (var-32) / 1.8;
+        return result;
+    }
+    double C_to_F(double var)
+    {
+        double result;
+        result = (var * 1.8) + 32;
+        return result;
+    }
+    double K_to_C(double var)
+    {
+        double result;
+        result = var - 273.15;
+        return result;
+    }
+    double C_to_K(double var)
+    {
+        double result;
+        result = var + 273.15;
+        return result;
+    }
+    double F_to_K(double var)
+    {
+        double result;
+        result = ((var - 32) / 1.8) + 273.15;
+        return result;
+    }
+    double K_to_F(double var)
+    {
+        double result;
+        result = ((var - 273.15) * 1.8) + 32;
+        return result;
+    }
+
+    void display(double result, char end_char){
+        TextView ans = (TextView) findViewById(R.id.textView);
+        ans.setText(Double.toString(result)+" "+"°"+end_char);
+    }
+
+    public double acceptvar(){
+        EditText editText = (EditText) findViewById(R.id.editText);
+        double var= Double.parseDouble(editText.getText().toString());
+
+
+        return var;
     }
 
 }
--- app/src/main/java/com/jr/poliv/temperatureconverter/Kelvin_to_Celsius.java
+++ app/src/main/java/com/jr/poliv/temperatureconverter/Kelvin_to_Celsius.java
@@ -1,13 +1,16 @@
 package com.jr.poliv.temperatureconverter;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 
-public class Kelvin_to_Celsius extends AppCompatActivity {
+public class Kelvin_to_Celsius extends Celsius_to_Fahrenheit {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +19,17 @@ public class Kelvin_to_Celsius extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                inputManager.hideSoftInputFromWindow((null == getCurrentFocus()) ? null : getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
+                display(K_to_C(acceptvar()), 'C');
             }
         });
+
     }
 
 }
